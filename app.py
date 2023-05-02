0e200fc2eae5ac11fb50f0883dd0bd9a5da9a7f5
monangchoz <96789453+monangchoz@users.noreply.github.com>
2023-05-02 21:44:16 +0700

    first

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from flask_ngrok import run_with_ngrok
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template("portofolio.html")
+
+if __name__ == '__main__':
+    run_with_ngrok(app)
+    app.run()
